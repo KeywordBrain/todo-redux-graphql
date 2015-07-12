e96caf3929f2047eac7706791dfcfa926bdcd001
David Pfahler <david@excellenteasy.com>
2015-07-12 16:59:40 +0200

    docs(reducers/data): describe functionality and possible improvements

--- reducers/data.js
+++ reducers/data.js
@@ -1,3 +1,8 @@
+// The data reducer receives only the state under
+// the "data" property key (such is the behavior of
+// all redux reducers). It transforms the data without
+// mutation.
+
 import { RECEIVE } from '../constants/ActionTypes'
 
 const initialState = {
@@ -8,6 +13,13 @@ const initialState = {
 export default function data(state = initialState, action) {
   switch (action.type) {
 
+  // The RECEIVE action type adds or updates data returned for
+  // GraphQL queries. It first creates a shallow copy of the
+  // data state (res and queries properties) and then adds
+  // or replaces the data for an (already existing or new) set
+  // of GraphQL queries.
+  // TODO: Investigate if below code can't be optimized by
+  // using some kind of map function.
   case RECEIVE:
     let newState = {
       ...state,
