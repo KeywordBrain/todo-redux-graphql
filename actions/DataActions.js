dc094a923a09c102a35cc7bc3bb8d17aa430f5ef
David Pfahler <david@excellenteasy.com>
2015-07-12 17:35:40 +0200

    docs(DataActions): clarify TODO

--- actions/DataActions.js
+++ actions/DataActions.js
@@ -1,11 +1,12 @@
 import { execQueries } from '../lib/exchange.js'
 import { RECEIVE } from '../constants/ActionTypes'
 
-// this has to receive the params as well as second arg
-// so we will be able to dispose of outdated observables
-export function executeQueries (queries) {
+// TODO: This action creator has to receive the params as well
+// as second argument so we will be able to dispose of outdated
+// observables (which should probably be managed by the exchange).
+export function executeQueries (queries, params = {}) {
   return dispatch => {
-    execQueries(queries).subscribe(res => {
+    execQueries(queries, params).subscribe(res => {
       dispatch({type: RECEIVE, res, queries})
     })
   }
