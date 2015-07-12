dd4f37183d3cdf019f70c3b2f5a960918686566e
David Pfahler <david@excellenteasy.com>
2015-07-12 12:37:43 +0200

    chore(webpack): notify of build errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
+var WebpackErrorNotificationPlugin = require('webpack-error-notification')
 
 module.exports = {
   devtool: 'eval',
@@ -15,7 +16,8 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new WebpackErrorNotificationPlugin()
   ],
   resolve: {
     extensions: ['', '.js']
@@ -30,4 +32,4 @@ module.exports = {
       loaders: ['style', 'raw']
     }]
   }
-};
+}
